2ba3da55784ba373b8279cca4a5758c004211370
pankaj-hpm <155936670+pankaj-hpm@users.noreply.github.com>
2024-04-18 13:26:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+
+	// 		<head>Feedback Form</head>
+
+	// <form method="post">
+		
+	// 	<label for="name">Name</label>
+	// 	<input id="name" type="text" minlength="3" maxlength="50" required>
+
+	// 	<label for="portfolio">Portfolio</label>
+	// 	<input id="portfolio" type="url" required>
+	// 	<label for="favColor">Favorite Color</label>
+	// 	<input id="favColor" type="color" >
+		
+	// 	<label for="linkeness">Linkeness</label>
+	// 	<input id="linkeness" type="range" min="1" max="100" required>
+		
+	// </form>
